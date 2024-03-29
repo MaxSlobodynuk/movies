a8ef1fa24eccdcefb66c7b4bfbcbcc2afe93effa
Max <slobodynuk.777@gmail.com>
2024-03-29 23:36:23 +0200

    add cast and reviews

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,28 +1,52 @@
 import { getCast } from 'moviesAPI';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Cast = () => {
-    const [cast, setCast] = useState(null);
-    const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+  const { movieId } = useParams();
 
-    useEffect(() => {
-      const getFilmCast = async () => {
-        try {
-          const {cast} = await getCast(movieId);
-          setCast(cast);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      getFilmCast();
-    }, [movieId]);
+  useEffect(() => {
+    const getFilmCast = async () => {
+      try {
+        const { cast } = await getCast(movieId);
+        setCast(cast);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getFilmCast();
+  }, [movieId]);
 
   console.log(cast);
-  
-  return (
-    <div>Cast</div>
-  )
-}
 
-export default Cast
+  return (
+    <section>
+      {cast.length > 0 ? (
+        <div style={{ marginLeft: '20px' }}>
+          {cast.map(item => (
+            <div
+              key={item.cast_id}
+              className="card mb-2"
+              style={{ maxWidth: '150px' }}
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500${item.profile_path}`}
+                className="card-img-top"
+                alt={item.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+                <p className="card-text">{item.character}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="card-text">Not found</p>
+      )}
+    </section>
+  );
+};
+
+export default Cast;
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Reviews = () => {
-  const [reviews, setReviews] = useState(null)
+  const [reviews, setReviews] = useState([])
   const { movieId } = useParams();
 
     useEffect(() => {
@@ -20,7 +20,24 @@ const Reviews = () => {
 
   console.log(reviews)
 
-  return <div>Reviews</div>;
+  return (
+    <section>
+      {reviews.length > 0 ? (
+        <ul>
+          {reviews.map(item => (
+            <li key={item.id}>
+              <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">{item.author}</h5>
+              </div>
+              <p className="mb-1">{item.content}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className='card-text'>No reviews</p>
+      )}
+    </section>
+  );
 };
 
 export default Reviews;
